6d3f94f14cec5f9204357822cc8f69254975da31
yuezaixz <303178394@qq.com>
2015-12-29 09:45:31 +0800

    日期，其实类似objc

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -309,3 +309,14 @@ case 15...30:
 default:
     print("other")
 }
+
+//日期
+var today :NSDate = NSDate()
+var format:NSDateFormatter = NSDateFormatter()
+//大写的YYYY应该是以本周的周六在哪年来计算的
+//比如，2015年12月29日是周二，本周的周六在2016年，所以"YYYY-MM-dd"->"2016-12-29"
+//总之，切记用小写的yyyy
+format.dateFormat = "YYYY-MM-dd"
+format.stringFromDate(today)
+format.dateFormat = "yyyy-MM-dd"
+format.stringFromDate(today)
